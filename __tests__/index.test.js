3588d52e86a152a1e1ea36e6075be21ab2a31897
Paul Morris <paul@paulwmorris.com>
2017-03-14 12:51:56 -0400

    Fix tests to work with refactoring work of previous commits

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,18 +7,6 @@ test('adds 1 + 1 to equal 2, just testing the testing setup', () => {
   expect(1 + 1).toBe(2);
 });
 
-import { addFigure } from '../src/actions';
-
-// let addFigure = require('../src/actions');
-
-test('addFigure action function', () => {
-    let result = addFigure('data');
-    expect(result).toEqual({
-        type: "ADD_FIGURE",
-        payload: 'data'
-    });
-});
-
 import { getStartEnd } from '../src/reducers';
 
 var figures = mori.toClj([
